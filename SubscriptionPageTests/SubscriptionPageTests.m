3d5db0736d8d4a1df8fdffdf1604f428d6bafec9
NewajShovo <leonelshovo@yahoo.com>
2019-04-04 17:45:40 +0600

    Subscription Page Creation

---
+++ SubscriptionPageTests/SubscriptionPageTests.m
@@ -0,0 +1,37 @@
+//
+//  SubscriptionPageTests.m
+//  SubscriptionPageTests
+//
+//  Created by Shafiq Shovo on 3/4/19.
+//  Copyright © 2019 Shafiq Shovo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SubscriptionPageTests : XCTestCase
+
+@end
+
+@implementation SubscriptionPageTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
